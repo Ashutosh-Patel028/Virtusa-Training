e66735289017d5a6200d6b4aa49379e8e494fba3
Ashutosh Patel <57192474+Ashutosh-Patel028@users.noreply.github.com>
2022-11-23 17:27:59 +0530

    added necessary files and folders

---
+++ Project Work/weatherApp-Reactjs/src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "22c7ae7758c5388da4b526880900d476",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
